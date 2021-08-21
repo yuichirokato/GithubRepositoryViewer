074e69c0a3881cc73f00ce7b97218d17511d9b00
Yuichiro Takahashi <ikmy0709@gmail.com>
2021-08-21 15:39:38 +0900

    Initial Commit

---
+++ GithubRepositoryViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubRepositoryViewer
+//
+//  Created by Yuichirou Takahashi on 2021/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubRepositoryViewer/GithubRepositoryViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryViewerApp.swift
+//  GithubRepositoryViewer
+//
+//  Created by Yuichirou Takahashi on 2021/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositoryViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
